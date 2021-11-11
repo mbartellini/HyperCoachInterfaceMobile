1bd2936014ce455584ac7b522facb4a0a68f6a4e
BraveJero <jbrave271@gmail.com>
2021-11-11 09:57:23 -0300

    add basic, no style, routine cards.

--- app/src/main/java/com/example/hypercoachinterface/ui/home/HomeFragment.java
+++ app/src/main/java/com/example/hypercoachinterface/ui/home/HomeFragment.java
@@ -11,14 +11,20 @@ import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 import androidx.lifecycle.Observer;
 import androidx.lifecycle.ViewModelProvider;
+import androidx.recyclerview.widget.LinearLayoutManager;
 
-import com.example.hypercoachinterface.R;
 import com.example.hypercoachinterface.databinding.FragmentHomeBinding;
+import com.example.hypercoachinterface.ui.home.adapter.ItemAdapter;
+
+import java.util.ArrayList;
 
 public class HomeFragment extends Fragment {
 
     private HomeViewModel homeViewModel;
     private FragmentHomeBinding binding;
+    private ArrayList<String> dataSet = new ArrayList<>();
+    private ItemAdapter adapter;
+
 
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
@@ -27,6 +33,25 @@ public class HomeFragment extends Fragment {
 
         binding = FragmentHomeBinding.inflate(inflater, container, false);
         View root = binding.getRoot();
+
+        for (int i = 1; i <= 50; i++)
+            dataSet.add("Item " + i);
+
+        adapter = new ItemAdapter(dataSet);
+
+        binding.myRoutinesView.setLayoutManager(new LinearLayoutManager(
+                this.getContext(),
+                LinearLayoutManager.HORIZONTAL,
+                false));
+        //binding.recyclerView.setLayoutManager(new GridLayoutManager(this, 3));
+        binding.myRoutinesView.setAdapter(adapter);
+
+        binding.allRoutinesView.setLayoutManager(new LinearLayoutManager(
+                this.getContext(),
+                LinearLayoutManager.HORIZONTAL,
+                false));
+        //binding.recyclerView.setLayoutManager(new GridLayoutManager(this, 3));
+        binding.allRoutinesView.setAdapter(adapter);
 
         final TextView textView = binding.textHome;
         homeViewModel.getText().observe(getViewLifecycleOwner(), new Observer<String>() {
