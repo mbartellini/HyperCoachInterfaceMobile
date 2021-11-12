edd4551e68a833d5c4b08c4d905be2d66d240fc1
shoni <juanigarcia@itba.edu.ar>
2021-11-12 12:33:54 -0300

    Add FragmentContainerView.

--- app/src/main/java/com/example/hypercoachinterface/MainActivity.java
+++ app/src/main/java/com/example/hypercoachinterface/MainActivity.java
@@ -7,6 +7,7 @@ import com.google.android.material.bottomnavigation.BottomNavigationView;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.navigation.NavController;
 import androidx.navigation.Navigation;
+import androidx.navigation.fragment.NavHostFragment;
 import androidx.navigation.ui.AppBarConfiguration;
 import androidx.navigation.ui.NavigationUI;
 
@@ -30,7 +31,9 @@ public class MainActivity extends AppCompatActivity {
                 R.id.navigation_home, R.id.navigation_search,
                 R.id.navigation_profile, R.id.navigation_favorites)
                 .build();
-        NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment_activity_main);
+//        NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment_activity_main);
+        NavHostFragment navHostFragment = (NavHostFragment) getSupportFragmentManager().findFragmentById(R.id.nav_host_fragment_activity_main);
+        NavController navController = navHostFragment.getNavController();
         NavigationUI.setupActionBarWithNavController(this, navController, appBarConfiguration);
         NavigationUI.setupWithNavController(binding.navView, navController);
     }
