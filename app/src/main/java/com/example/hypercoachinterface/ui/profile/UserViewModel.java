29ba78571943ab81f6e2ad5fd98fa488d1b2a553
Mateo Bartellini Huapalla <mbartellini@itba.edu.ar>
2021-11-15 11:42:49 -0300

    Remove unnecesary imports

--- app/src/main/java/com/example/hypercoachinterface/ui/profile/UserViewModel.java
+++ app/src/main/java/com/example/hypercoachinterface/ui/profile/UserViewModel.java
@@ -1,18 +1,11 @@
 package com.example.hypercoachinterface.ui.profile;
 
 import androidx.lifecycle.LiveData;
-import androidx.lifecycle.MediatorLiveData;
-import androidx.lifecycle.MutableLiveData;
-import androidx.lifecycle.Transformations;
-import androidx.lifecycle.ViewModel;
 
 import com.example.hypercoachinterface.backend.api.model.User;
 import com.example.hypercoachinterface.backend.repository.Resource;
 import com.example.hypercoachinterface.backend.repository.UserRepository;
 import com.example.hypercoachinterface.viewmodel.RepositoryViewModel;
-
-import java.util.ArrayList;
-import java.util.List;
 
 public class UserViewModel extends RepositoryViewModel<UserRepository> {
 
