a735df4c3c5e2696fc9495dda2324a01e0f47499
Mateo Bartellini Huapalla <mbartellini@itba.edu.ar>
2021-11-17 14:49:08 -0300

    Work

--- app/src/main/java/com/example/hypercoachinterface/ui/routine/RoutineDetailActivity.java
+++ app/src/main/java/com/example/hypercoachinterface/ui/routine/RoutineDetailActivity.java
@@ -6,21 +6,48 @@ import android.os.Bundle;
 import android.util.Log;
 
 import com.example.hypercoachinterface.R;
+import com.example.hypercoachinterface.backend.App;
+import com.example.hypercoachinterface.backend.api.model.Routine;
+import com.example.hypercoachinterface.backend.repository.Status;
+import com.example.hypercoachinterface.databinding.ActivityMainBinding;
+import com.example.hypercoachinterface.databinding.ActivityRoutineDetailBinding;
 
 public class RoutineDetailActivity extends AppCompatActivity {
 
     private static final String TAG = "RoutineDetailActivity";
+    private ActivityRoutineDetailBinding binding;
+    private App app;
     private int routineId = -1;
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_routine_detail);
 
-        Bundle b = getIntent().getExtras();
-        if(b != null)
-            routineId = b.getInt("routineId");
+        app = (App) getApplication();
 
+        binding = ActivityRoutineDetailBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+
+        Bundle b = getIntent().getExtras();
+        if (b == null) {
+            // TODO: Handle error
+            // Volver a la página principal mostrando un error de invalid id
+        }
+        routineId = b.getInt("routineId");
         Log.d(TAG, "onCreate: " + String.valueOf(routineId));
 
+
+        app.getRoutineRepository().getRoutine(routineId).observe(this, r -> {
+            if (r.getStatus() == Status.SUCCESS) {
+                fillActivityData(r.getData());
+            }
+        });
+
+    }
+
+    private void fillActivityData(Routine routine) {
+        binding.routineTitle.setText(routine.getName());
     }
 }
