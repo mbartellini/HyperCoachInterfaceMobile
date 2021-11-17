03f4d0857df5aa1c2fdbc1475984cccaafd62804
Mateo Bartellini Huapalla <mbartellini@itba.edu.ar>
2021-11-17 13:49:57 -0300

    Create empty activity

---
+++ app/src/main/java/com/example/hypercoachinterface/ui/routine/RoutineDetailActivity.java
@@ -0,0 +1,16 @@
+package com.example.hypercoachinterface.ui.routine;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.hypercoachinterface.R;
+
+public class RoutineDetailActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_routine_detail);
+    }
+}
