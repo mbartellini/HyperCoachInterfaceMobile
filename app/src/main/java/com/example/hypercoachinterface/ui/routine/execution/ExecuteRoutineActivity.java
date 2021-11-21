48d4cd58bb3ce13c7184624405cb459e575b7ff1
Mateo Bartellini Huapalla <mbartellini@itba.edu.ar>
2021-11-20 23:27:39 -0300

    Add immersive execution

--- app/src/main/java/com/example/hypercoachinterface/ui/routine/execution/ExecuteRoutineActivity.java
+++ app/src/main/java/com/example/hypercoachinterface/ui/routine/execution/ExecuteRoutineActivity.java
@@ -72,6 +72,9 @@ public class ExecuteRoutineActivity extends AppCompatActivity {
         binding = ActivityExecuteRoutineBinding.inflate(getLayoutInflater());
         setContentView(binding.getRoot());
 
+        int UI_OPTIONS = View.SYSTEM_UI_FLAG_HIDE_NAVIGATION | View.SYSTEM_UI_FLAG_FULLSCREEN | View.SYSTEM_UI_FLAG_LAYOUT_STABLE | View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY | View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION;
+        getWindow().getDecorView().setSystemUiVisibility(UI_OPTIONS);
+
         if (savedInstanceState != null) {
             currentExercise = savedInstanceState.getInt("currentExercise");
             currentCycle = savedInstanceState.getInt("currentCycle");
