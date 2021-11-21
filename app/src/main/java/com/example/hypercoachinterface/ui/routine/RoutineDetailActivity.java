c9baf83908c5b59da693f3cc7ad0073160a64033
BraveJero <jbrave271@gmail.com>
2021-11-20 23:18:00 -0300

    my life appears to have *some* meaning

--- app/src/main/java/com/example/hypercoachinterface/ui/routine/RoutineDetailActivity.java
+++ app/src/main/java/com/example/hypercoachinterface/ui/routine/RoutineDetailActivity.java
@@ -9,6 +9,7 @@ import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
